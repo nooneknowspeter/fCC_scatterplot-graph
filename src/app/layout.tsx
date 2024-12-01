5a8991293136b7e3f55a373d06c180490f1503e3
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-01 19:20:08 -0400

    npm i theme-change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="luxury">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
