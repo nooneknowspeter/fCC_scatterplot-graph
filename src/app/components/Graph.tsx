59be0abff76f14444194965f861da61dec3533f1
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-01 01:49:45 -0400

    added Graph component
    setup junction file for centralized imp and exp

---
+++ src/app/components/Graph.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Graph = () => {
+  return (
+    <>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+      <div>Graph</div>
+    </>
+  );
+};
+
+export default Graph;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Graph } from "./components/Centralized";
+
 export default function Home() {
   return (
     <>
       <h1 className="font-bold text-center">hello world</h1>
+      <Graph />
     </>
   );
 }
