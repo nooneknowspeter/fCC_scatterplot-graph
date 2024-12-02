fb47235cef661b0d3a253c9d3bf5a7bf099236e4
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-02 01:37:48 -0400

    dynamically populate radio buttons for themes

--- src/app/components/ThemeController.tsx
+++ src/app/components/ThemeController.tsx
@@ -15,66 +15,19 @@ const ThemeController = () => {
         id="theme-controller"
         className="absolute right-0 top-0 m-2 w-40 transition-all"
       >
-        {/* default */}
-        <div className="form-control">
-          <label className="label cursor-pointer gap-4">
-            <span className="label-text">Dark</span>
-            <input
-              type="radio"
-              name="theme-radios"
-              className="radio theme-controller"
-              value="black"
-            />
-          </label>
-        </div>
-        {/* theme 1 */}
-        <div className="form-control">
-          <label className="label cursor-pointer gap-4">
-            <span className="label-text">Retro</span>
-            <input
-              type="radio"
-              name="theme-radios"
-              className="radio theme-controller"
-              value="retro"
-            />
-          </label>
-        </div>
-        {/* theme 2 */}
-        <div className="form-control">
-          <label className="label cursor-pointer gap-4">
-            <span className="label-text">Cyberpunk</span>
-            <input
-              type="radio"
-              name="theme-radios"
-              className="radio theme-controller"
-              value="cyberpunk"
-            />
-          </label>
-        </div>
-        {/* theme 3 */}
-        <div className="form-control">
-          <label className="label cursor-pointer gap-4">
-            <span className="label-text">Winter</span>
-            <input
-              type="radio"
-              name="theme-radios"
-              className="radio theme-controller"
-              value="winter"
-            />
-          </label>
-        </div>
-        {/* theme 4 */}
-        <div className="form-control">
-          <label className="label cursor-pointer gap-4">
-            <span className="label-text">Luxury</span>
-            <input
-              type="radio"
-              name="theme-radios"
-              className="radio theme-controller"
-              value="luxury"
-            />
-          </label>
-        </div>
+        {themes.map((theme) => (
+          <div key={theme.value} className="form-control">
+            <label className="label cursor-pointer gap-4">
+              <span className="label-text">{theme.name}</span>
+              <input
+                type="radio"
+                name="theme-radios"
+                className="radio theme-controller"
+                value={theme.value}
+              />
+            </label>
+          </div>
+        ))}
       </div>
     </>
   );
