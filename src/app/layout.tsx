0910a587926b1c7113342bebeeef7d518d1d9e16
peter <157999171+nooneknowspeter@users.noreply.github.com>
2024-12-04 21:41:37 -0400

    Update layout.tsx
    commented fCC CDN test link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
           src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
           defer
-        ></script>
+        ></script> */}
       </head>
       <body className={`${spaceGrotesk.variable} antialiased`}>{children}</body>
     </html>
